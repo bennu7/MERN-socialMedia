24e8c1d1da4e06f27973bf45cf261da14cf04d2b
bennu7 <laluibnuhidayatullah@gmail.com>
2022-11-01 20:49:55 +0700

    right menu slicing UI

--- frontend/src/components/rightBar/rightBar.jsx
+++ frontend/src/components/rightBar/rightBar.jsx
@@ -2,7 +2,121 @@ import React from "react";
 import "./rightBar.scss";
 
 const RightBar = () => {
-	return <div className="rightBar">RightBar</div>;
+	return (
+		<div className="rightBar">
+			<div className="container">
+				<div className="item">
+					<span>Suggestions for you</span>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<span>Lalu Ibnu Hidyatullah</span>
+						</div>
+						<div className="buttons">
+							<button>follow</button>
+							<button>dismiss</button>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<span>Lalu Ibnu Hidyatullah</span>
+						</div>
+						<div className="buttons">
+							<button>follow</button>
+							<button>dismiss</button>
+						</div>
+					</div>
+				</div>
+				<div className="item">
+					<span>Latest Activities</span>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<p>
+								<span>Lalu Ibnu </span>changed profile picture
+							</p>
+						</div>
+						<span>1 min ago</span>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<p>
+								<span>Lalu Ibnu </span>changed profile picture
+							</p>
+						</div>
+						<span>1 min ago</span>
+					</div>
+				</div>
+				<div className="item">
+					<span>Online Friends</span>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<div className="online" />
+							<span>Lalu Ibnu </span>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<div className="online" />
+							<span>Lalu Ibnu </span>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<div className="online" />
+							<span>Lalu Ibnu </span>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<div className="online" />
+							<span>Lalu Ibnu </span>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img
+								src="https://images.pexels.com/photos/12627287/pexels-photo-12627287.jpeg?cs=srgb&dl=pexels-norma-gabriela-galván-12627287.jpg&fm=jpg&_gl=1*l26btu*_ga*MTQ0NDIwMzU5OC4xNjY3MDIyMjU2*_ga_8JE65Q40S6*MTY2NzAyMjI1Ny4xLjEuMTY2NzAyMjM5MC4wLjAuMA.."
+								alt=""
+							/>
+							<div className="online" />
+							<span>Lalu Ibnu </span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default RightBar;
